dca0f08911bc01103a622ded638d197fd60ad2e4
johnconh <sevenballs@gmail.com>
2023-05-02 18:07:49 +0200

    actualizado

--- srcs/free_exit.c
+++ srcs/free_exit.c
@@ -6,7 +6,7 @@
 /*   By: jdasilva <jdasilva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 16:37:33 by jdasilva          #+#    #+#             */
-/*   Updated: 2023/05/02 17:35:58 by jdasilva         ###   ########.fr       */
+/*   Updated: 2023/05/02 18:07:34 by jdasilva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,25 @@
 
 void	ft_texture_free(t_map *map, int flag)
 {
-	if (map->NO_img)
+	if (map->no_img)
 	{
-		free(map->NO_img->img);
-		free(map->NO_img);
+		free(map->no_img->img);
+		free(map->no_img);
 	}
-	if (map->SO_img)
+	if (map->so_img)
 	{
-		free(map->SO_img->img);
-		free(map->SO_img);
+		free(map->so_img->img);
+		free(map->so_img);
 	}
-	if (map->EA_img)
+	if (map->ea_img)
 	{
-		free(map->EA_img->img);
-		free(map->EA_img);
+		free(map->ea_img->img);
+		free(map->ea_img);
 	}
-	if (map->WE_img)
+	if (map->we_img)
 	{
-		free(map->WE_img->img);
-		free(map->WE_img);
+		free(map->we_img->img);
+		free(map->we_img);
 	}
 	free(map);
 	if (flag == 1)

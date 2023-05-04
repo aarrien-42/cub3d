10ae28a5ab47d2bce28af8c503d8b17d53bca7f0
johnconh <sevenballs@gmail.com>
2023-05-04 16:10:41 +0200

    normi ok

--- srcs/free_exit.c
+++ srcs/free_exit.c
@@ -6,7 +6,7 @@
 /*   By: jdasilva <jdasilva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 16:37:33 by jdasilva          #+#    #+#             */
-/*   Updated: 2023/05/04 15:51:33 by jdasilva         ###   ########.fr       */
+/*   Updated: 2023/05/04 16:10:23 by jdasilva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@ void	ft_texture_free2(t_map *map)
 	if (map->ea_img)
 	{
 		free(map->ea_img->img);
-		if(map->ea_img->addr)
+		if (map->ea_img->addr)
 			free(map->ea_img->addr);
 		free(map->ea_img);
 	}
 	if (map->we_img)
 	{
 		free(map->we_img->img);
-		if(map->we_img->addr)
+		if (map->we_img->addr)
 			free(map->we_img->addr);
 		free(map->we_img);
 	}
@@ -35,14 +35,14 @@ void	ft_texture_free(t_map *map, int flag)
 	if (map->no_img)
 	{
 		free(map->no_img->img);
-		if(map->no_img->addr)
+		if (map->no_img->addr)
 			free(map->no_img->addr);
 		free(map->no_img);
 	}
 	if (map->so_img)
 	{
 		free(map->so_img->img);
-		if(map->so_img->addr)
+		if (map->so_img->addr)
 			free(map->so_img->addr);
 		free(map->so_img);
 	}
